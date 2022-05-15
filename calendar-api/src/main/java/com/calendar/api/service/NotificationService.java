cc4d404a9d765bfe17f2bf43217a3e1333c78cb3
hoon <moradulee@gmail.com>
2022-05-15 23:10:45 +0900

    Notification Domain

---
+++ calendar-api/src/main/java/com/calendar/api/service/NotificationService.java
@@ -0,0 +1,40 @@
+package com.calendar.api.service;
+
+import com.calendar.api.dto.AuthUser;
+import com.calendar.api.dto.NotificationCreateReq;
+import com.calendar.core.domain.entity.Schedule;
+import com.calendar.core.domain.entity.User;
+import com.calendar.core.domain.entity.repository.ScheduleRepository;
+import com.calendar.core.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final UserService userService;
+    private final ScheduleRepository scheduleRepository;
+
+    @Transactional
+    public void create(
+            NotificationCreateReq notificationCreateReq,
+            AuthUser authUser
+    ) {
+        final User user = userService.findByUserId(authUser.getId());
+        final List<LocalDateTime> notifyAts = notificationCreateReq.getRepeatTimes();
+        notifyAts.forEach(notifyAt -> {
+            final Schedule notificationSchedule =
+                    Schedule.notification(
+                            notificationCreateReq.getTitle(),
+                            notifyAt,
+                            user
+                    );
+            scheduleRepository.save(notificationSchedule);
+        });
+    }
+}
---
+++ calendar-core/src/main/java/com/calendar/core/util/TimeUnit.java
@@ -0,0 +1,5 @@
+package com.calendar.core.util;
+
+public enum TimeUnit {
+    DAY, WEEK, MONTH, YEAR
+}

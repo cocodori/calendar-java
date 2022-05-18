1eb924c782f84df206c48c4518bbe8c2e2c82369
hoon <moradulee@gmail.com>
2022-05-18 23:30:32 +0900

    validation

--- calendar-api/src/main/java/com/calendar/api/controller/LoginApiController.java
+++ calendar-api/src/main/java/com/calendar/api/controller/LoginApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +18,7 @@ public class LoginApiController {
     private final LoginService loginService;
 
     @PostMapping("/api/sign-up")
-    public ResponseEntity<Void> signUp(@RequestBody SignUpReq signUpReq, HttpSession session) {
+    public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpReq signUpReq, HttpSession session) {
         loginService.signUp(signUpReq, session);
         return ResponseEntity.ok().build();
     }

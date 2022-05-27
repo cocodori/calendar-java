799066ef855aa88b11a8a322655304655c55643b
hoon <moradulee@gmail.com>
2022-05-27 23:56:59 +0900

    이메일 템플릿2

--- calendar-api/src/main/java/com/calendar/api/dto/EngagementEmailStuff.java
+++ calendar-api/src/main/java/com/calendar/api/dto/EngagementEmailStuff.java
@@ -3,20 +3,45 @@ package com.calendar.api.dto;
 import com.calendar.core.util.Period;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.util.Pair;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 @Data
 @Builder
 public class EngagementEmailStuff {
     private static final String engagementUpdateApi = "http://localhost:8080/events/engagements/";
+    public static final String MAIL_TIME_FORMAT = "yyyy년 MM월 dd일(E) a hh시 mm분";
+    public static final List<Pair<String, Predicate<Period>>> endAtFormatParts = Arrays.asList(
+            Pair.of("yyyy년 ", period -> period.getEndAt().getYear() == period.getStartAt().getYear()),
+            Pair.of("MM월 ", period -> period.getEndAt().getMonth() == period.getStartAt().getMonth()),
+            Pair.of("dd일(E) ", period -> period.getEndAt().getDayOfMonth() == period.getStartAt().getDayOfMonth())
+    );
     private final Long engagementId;
     private final String toEmail;
     private final List<String> attendeeEmails;
     private final String title;
     private final Period period;
+
+    public static String getEndAtFormat(Period period,
+                                        String format,
+                                        List<Pair<String,Predicate<Period>>> remainEndAtFormatParts) {
+        if (remainEndAtFormatParts.isEmpty()) {
+            return format;
+        } else if (remainEndAtFormatParts.get(0).getSecond().test(period)) {
+            return getEndAtFormat(
+                    period,
+                    format.replace(remainEndAtFormatParts.get(0).getFirst(), ""),
+                    remainEndAtFormatParts.subList(1, remainEndAtFormatParts.size())
+            );
+        } else {
+            return format;
+        }
+    }
 
     public String getSubject() {
         return new StringBuilder()
